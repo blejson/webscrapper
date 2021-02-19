fb3fc5f66d8303bada8f8910647c22b7282ebd90
Jacek B <jaceble@interia.eu>
2021-02-19 22:27:31 +0100

    Images download added

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import requests
-
+from bs4 import BeautifulSoup
+from PIL import Image
+from io import BytesIO
 search = input("Search for:")
 params = {"q": search}
 r = requests.get("https://www.bing.com/images/search", params=params)
-
-print(r.text)
+soup = BeautifulSoup(r.text, "html.parser")
+links = soup.findAll("a", {"class": "thumb"})
+for item in links:
+    img_obj = requests.get(item.attrs["href"])
+    title = item.attrs["href"].split("/")[-1]
+    img = Image.open(BytesIO(img_obj.content))
+    img.save("./scraped_images/"+title, img.format)
